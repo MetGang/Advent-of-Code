9cd00612f3d2491b79f51ba276f6d2fe1ee61026
MetGang <patrick_stritch@wp.pl>
2021-12-12 19:31:17 +0100

    Update 09/2021

--- 2021/09/p2.py
+++ 2021/09/p2.py
@@ -1,17 +1,17 @@
 # https://adventofcode.com/2021/day/9
 
-def traverse(mat, i, j, w, h):
-	mat[j][i] = 9
+def traverse(matrix, i, j, w, h):
+	matrix[j][i] = 9
 	total = 1
 
-	if j > 0 and mat[j - 1][i] != 9:
-		total += traverse(mat, i, j - 1, w, h)
-	if i > 0 and mat[j][i - 1] != 9:
-		total += traverse(mat, i - 1, j, w, h)
-	if j < h - 1 and mat[j + 1][i] != 9:
-		total += traverse(mat, i, j + 1, w, h)
-	if i < w - 1 and mat[j][i + 1] != 9:
-		total += traverse(mat, i + 1, j, w, h)
+	if j > 0 and matrix[j - 1][i] != 9:
+		total += traverse(matrix, i, j - 1, w, h)
+	if i > 0 and matrix[j][i - 1] != 9:
+		total += traverse(matrix, i - 1, j, w, h)
+	if j < h - 1 and matrix[j + 1][i] != 9:
+		total += traverse(matrix, i, j + 1, w, h)
+	if i < w - 1 and matrix[j][i + 1] != 9:
+		total += traverse(matrix, i + 1, j, w, h)
 
 	return total
 
