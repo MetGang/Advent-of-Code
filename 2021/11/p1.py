dbc8874da6246bb3cba5e50edabd1670770c0137
MetGang <patrick_stritch@wp.pl>
2021-12-12 19:58:43 +0100

    Update 11/2021

--- 2021/11/p1.py
+++ 2021/11/p1.py
@@ -5,47 +5,47 @@ import numpy as np
 INT32_MIN = np.iinfo(np.int32).min
 
 def solve(data):
-	h, w = np.shape(data)
-	total = 0
+    h, w = np.shape(data)
+    total = 0
 
-	for _ in range(100):
-		data += 1
+    for _ in range(100):
+        data += 1
 
-		while not np.all(data < 10):
-			for j in range(h):
-				for i in range(w):
-					if data[j][i] > 9:
-						data[j][i] = INT32_MIN
+        while not np.all(data < 10):
+            for j in range(h):
+                for i in range(w):
+                    if data[j][i] > 9:
+                        data[j][i] = INT32_MIN
 
-						if j > 0 and i > 0:
-							data[j - 1][i - 1] += 1
-						if j > 0:
-							data[j - 1][i] += 1
-						if j > 0 and i < w - 1:
-							data[j - 1][i + 1] += 1
-						if i > 0:
-							data[j][i - 1] += 1
-						if i < w - 1:
-							data[j][i + 1] += 1
-						if j < h - 1 and i > 0:
-							data[j + 1][i - 1] += 1
-						if j < h - 1:
-							data[j + 1][i] += 1
-						if j < h - 1 and i < w - 1:
-							data[j + 1][i + 1] += 1
+                        if j > 0 and i > 0:
+                            data[j - 1][i - 1] += 1
+                        if j > 0:
+                            data[j - 1][i] += 1
+                        if j > 0 and i < w - 1:
+                            data[j - 1][i + 1] += 1
+                        if i > 0:
+                            data[j][i - 1] += 1
+                        if i < w - 1:
+                            data[j][i + 1] += 1
+                        if j < h - 1 and i > 0:
+                            data[j + 1][i - 1] += 1
+                        if j < h - 1:
+                            data[j + 1][i] += 1
+                        if j < h - 1 and i < w - 1:
+                            data[j + 1][i + 1] += 1
 
-		mask = data < 0
+        mask = data < 0
 
-		total += mask.sum()
+        total += mask.sum()
 
-		data[mask] = 0
+        data[mask] = 0
 
-	return total
+    return total
 
 def parse(content):
-	return np.array([ np.array([ np.int32(char) for char in line ]) for line in content.split('\n') ])
+    return np.array([ np.array([ np.int32(char) for char in line ]) for line in content.split('\n') ])
 
 with open('input.txt') as file:
-	data = parse(file.read()[:-1])
+    data = parse(file.read()[:-1])
 
-	print(solve(data))
+    print(solve(data))
