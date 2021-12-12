a4bdc2c9bfe49e94df7f4169e6eda6dcbe44eafb
MetGang <patrick_stritch@wp.pl>
2021-12-12 19:58:52 +0100

    Update 12/2021

--- 2021/12/p2.py
+++ 2021/12/p2.py
@@ -1,44 +1,44 @@
 # https://adventofcode.com/2021/day/12
 
 def solve(data):
-	paths = [ ([ 'start' ], True) ]
-	total = 0
+    paths = [ ([ 'start' ], True) ]
+    total = 0
 
-	while paths:
-		path, can_route = paths.pop()
-		connections = data[path[-1]]
+    while paths:
+        path, can_route = paths.pop()
+        connections = data[path[-1]]
 
-		for c in connections:
-			if c == 'start':
-				continue
-			elif c == 'end':
-				total += 1
-			elif (c not in path) or (c.isupper()):
-				paths.append(([ *path, c ], can_route))
-			elif can_route:
-				paths.append(([ *path, c ], False))
+        for c in connections:
+            if c == 'start':
+                continue
+            elif c == 'end':
+                total += 1
+            elif (c not in path) or (c.isupper()):
+                paths.append(([ *path, c ], can_route))
+            elif can_route:
+                paths.append(([ *path, c ], False))
 
-	return total
+    return total
 
 def parse(content):
-	nodes = dict()
+    nodes = dict()
 
-	for line in content.split('\n'):
-		first, second = line.split('-')
+    for line in content.split('\n'):
+        first, second = line.split('-')
 
-		if first not in nodes:
-			nodes[first] = []
+        if first not in nodes:
+            nodes[first] = []
 
-		nodes[first].append(second)
+        nodes[first].append(second)
 
-		if second not in nodes:
-			nodes[second] = []
+        if second not in nodes:
+            nodes[second] = []
 
-		nodes[second].append(first)
+        nodes[second].append(first)
 
-	return nodes
+    return nodes
 
 with open('input.txt') as file:
-	data = parse(file.read()[:-1])
+    data = parse(file.read()[:-1])
 
-	print(solve(data))
+    print(solve(data))
