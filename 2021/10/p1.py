379604f4703a554779426c379192f3ac930ad491
MetGang <patrick_stritch@wp.pl>
2021-12-12 19:58:34 +0100

    Update 10/2021

--- 2021/10/p1.py
+++ 2021/10/p1.py
@@ -5,28 +5,28 @@ CHAR_MAPPING = { ')': '(', ']': '[', '}': '{', '>': '<' }
 POINTS_MAPPING = { ')': 3, ']': 57, '}': 1197, '>': 25137 }
 
 def solve(data):
-	points = []
+    points = []
 
-	for entry in data:
-		stack = []
+    for entry in data:
+        stack = []
 
-		for ch in entry:
-			if ch in [ '(', '[', '{', '<' ]:
-				stack.append(ch)
-			else:
-				if stack[-1] != CHAR_MAPPING[ch]:
-					points.append(POINTS_MAPPING[ch])
-					break
-				else:
-					stack.pop()
+        for ch in entry:
+            if ch in [ '(', '[', '{', '<' ]:
+                stack.append(ch)
+            else:
+                if stack[-1] != CHAR_MAPPING[ch]:
+                    points.append(POINTS_MAPPING[ch])
+                    break
+                else:
+                    stack.pop()
 
-	return sum(points)
+    return sum(points)
 
 def parse(content):
-	return [ line for line in content.split('\n') ]
+    return [ line for line in content.split('\n') ]
 
 with open('input.txt') as file:
-	data = parse(file.read()[:-1])
+    data = parse(file.read()[:-1])
 
-	print(solve(data))
+    print(solve(data))
 
--- 2021/10/p2.py
+++ 2021/10/p2.py
@@ -5,37 +5,37 @@ CHAR_MAPPING = { ')': '(', ']': '[', '}': '{', '>': '<' }
 VALUES_MAPPING = { '(': 1, '[': 2, '{': 3, '<': 4 }
 
 def solve(data):
-	results = []
+    results = []
 
-	for entry in data:
-		stack = []
+    for entry in data:
+        stack = []
 
-		for ch in entry:	
-			if ch in [ '(', '[', '{', '<' ]:
-				stack.append(ch)
-			else:
-				if stack[-1] != CHAR_MAPPING[ch]:
-					break
-				else:
-					stack.pop()
-		else:
-			stack.reverse()
-			points = 0
+        for ch in entry:	
+            if ch in [ '(', '[', '{', '<' ]:
+                stack.append(ch)
+            else:
+                if stack[-1] != CHAR_MAPPING[ch]:
+                    break
+                else:
+                    stack.pop()
+        else:
+            stack.reverse()
+            points = 0
 
-			for value in stack:
-				points *= 5
-				points += VALUES_MAPPING[value]
+            for value in stack:
+                points *= 5
+                points += VALUES_MAPPING[value]
 
-			results.append(points)
+            results.append(points)
 
-	results.sort()
+    results.sort()
 
-	return results[len(results) // 2]
+    return results[len(results) // 2]
 
 def parse(content):
-	return [ line for line in content.split('\n') ]
+    return [ line for line in content.split('\n') ]
 
 with open('input.txt') as file:
-	data = parse(file.read()[:-1])
+    data = parse(file.read()[:-1])
 
-	print(solve(data))
+    print(solve(data))
