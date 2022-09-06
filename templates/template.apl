f57930d8072cb62bdee40fb48252f616c332f42b
MetGang <patrick_stritch@wp.pl>
2022-09-06 23:32:46 +0200

    Add code templates

---
+++ templates/template.apl
@@ -0,0 +1,9 @@
+⍝ https://adventofcode.com/YYYY/day/DD
+
+rawData ← ⊃⎕NGET'input.txt'1
+
+data ← rawData
+
+Solve ← ⊢
+
+Solve data
---
+++ templates/template.py
@@ -0,0 +1,12 @@
+# https://adventofcode.com/YYYY/day/DD
+
+def solve(data):
+    return data
+
+def parse(content):
+    return content
+
+with open('input.txt') as file:
+    data = parse(file.read().strip())
+
+    print(solve(data))
