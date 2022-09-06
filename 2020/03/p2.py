2388d059156fb70cdddc1a11bcc8a6ae2763a1f0
MetGang <patrick_stritch@wp.pl>
2022-09-06 23:27:08 +0200

    Update 03/2020

---
+++ 2020/03/p2.py
@@ -0,0 +1,24 @@
+# https://adventofcode.com/2020/day/3
+
+def traverse(data, slope_x, slope_y):
+    count = 0
+    x = 0
+    y = 0
+
+    while y < len(data):
+        count += data[y][x % len(data[y])] == '#'
+        x += slope_x
+        y += slope_y
+
+    return count
+
+def solve(data):
+    return traverse(data, 1, 1) * traverse(data, 3, 1) * traverse(data, 5, 1) * traverse(data, 7, 1) * traverse(data, 1, 2)
+
+def parse(content):
+    return content.split('\n')
+
+with open('input.txt') as file:
+    data = parse(file.read().strip())
+
+    print(solve(data))
